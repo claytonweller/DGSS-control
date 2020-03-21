de0f244b32ce888f633641faddc3cf562b752bc3
ClaytonWeller <clayton.weller@gmail.com>
2020-03-21 10:09:44 -0600

    Made it a react app

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>It's working</h1>
+      </div>
+    );
+  }
+}
