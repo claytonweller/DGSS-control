e306f3feb3ee52c298d1abd7256eadd525946a8b
ClaytonWeller <clayton.weller@gmail.com>
2020-04-19 13:37:37 -0600

    Cleaned up action manager

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { client } from '../index'
+import { performanceActionHash } from './performances'
+import { utilityActionHash } from './utility'
 
 export const manageMessage = async (message, component) => {
   let action = Object.keys(actionHash).includes(message.action)
@@ -13,40 +14,9 @@ export const manageMessage = async (message, component) => {
 }
 
 const actionHash = {
-  'local-server': localServerAction,
-  'conn-update': connectionUpdateAction,
-  'performance-created': performanceCreatedAction,
-  'performance-ended': performanceEndedAction,
-  'performance-joined': performanceJoinedAction,
+  ...performanceActionHash,
+  ...utilityActionHash,
   defaultAction
 }
 
 async function defaultAction(params, component) { console.log('DEFAULT \n', params, component.state) }
-
-async function performanceJoinedAction(params, component) {
-  console.log(`${params.attendee.name} joined the audience`)
-}
-
-async function performanceCreatedAction(params, component) {
-  console.log('Performance Created \n', params)
-  component.setState({ performance: params })
-}
-
-async function performanceEndedAction(parms, component) {
-  console.log('Performance Ended')
-  component.setState({ performance: {} })
-}
-
-async function localServerAction(params, component) {
-  console.log('local-server\n', params)
-  const sendParams = {
-    source: 'control'
-  }
-  client.send(JSON.stringify({ action: 'connect-source', params: sendParams }))
-  component.setState({ currentConn: params })
-}
-
-async function connectionUpdateAction(params, component) {
-  console.log('conn-update\n', params)
-  component.setState({ currentConn: params.currentConnection })
-}
