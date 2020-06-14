3ad8aa8e54827f906b5872f3d79994a77f4235c5
Clayton <clayton.weller@gmail.com>
2020-06-14 14:34:06 -0600

    Trolly Question Maestro Logic working

--- src/components/modules/Trolly/index.js
+++ src/components/modules/Trolly/index.js
@@ -21,10 +21,10 @@ export function Trolly({ moduleState, sendAction }) {
     return (
       <div>
         <div>
-          <button onClick={() => questionClick('5 people', '1 person')}>1 Basic</button>
-          <button onClick={() => questionClick('5 people', 'Snipe Trolly driver')}>2 Sniper</button>
-          <button onClick={() => questionClick('5 people', 'Shove person in front of Trolly')}>3 Shove</button>
-          <button onClick={() => questionClick('1 baby', '1 adult', 1750)}>4 Timer</button>
+          <button onClick={() => questionClick('5 people', '1 person', 999999)}>1 Basic</button>
+          <button onClick={() => questionClick('5 people', 'Snipe Trolly driver', 999999)}>2 Sniper</button>
+          <button onClick={() => questionClick('5 people', 'Shove person in front of Trolly', 999999)}>3 Shove</button>
+          <button onClick={() => questionClick('1 baby', '1 adult', 7750)}>4 Timer</button>
         </div>
         <div>
           <button onClick={() => sendAction('trolly-madness')}>Madness</button>
